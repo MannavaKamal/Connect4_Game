ea6d40ca2d9bccdce260a3de67e7bfa9c704f636
Kamal Mannava <141606700+MannavaKamal@users.noreply.github.com>
2025-10-23 06:52:19 +0530

    Update useMultiplayer.ts

--- frontend/src/hooks/useMultiplayer.ts
+++ frontend/src/hooks/useMultiplayer.ts
@@ -93,22 +93,24 @@ export const useMultiplayer = (onOpponentMove?: (column: number) => void) => {
           }
      else if(res.chance == 1){ 
       setMoveTrigger((t) => {
-        const rv = t + generateRandom()
-        console.log("in 2r "+rv)
-        return rv });
-       setCol(-1)            
-       setMoveTrigger((t) => {
-        const rv = t + generateRandom()
-         console.log("in 3r "+rv)
-        return rv });
+       //  const rv = t + generateRandom()
+       //  console.log("in 2r "+rv)
+       //  return rv });
+       // setCol(-1)            
+       // setMoveTrigger((t) => {
+       //  const rv = t + generateRandom()
+       //   console.log("in 3r "+rv)
+       //  return rv });
+        console.log("chance is 1")
       } else if(res.chance == 0){
-       setMoveTrigger((t) => {
-        const rv = t + generateRandom()
-         console.log("in 4r "+rv)
-        return rv });
-        console.log("in 0")
-        setCol(-1)
-        setCurrentPlayer(2)
+       // setMoveTrigger((t) => {
+       //  const rv = t + generateRandom()
+       //   console.log("in 4r "+rv)
+       //  return rv });
+       //  console.log("in 0")
+       //  setCol(-1)
+       //  setCurrentPlayer(2)
+        console.log("chance is 1")
       }
     }else if(res.type === 22){
       if(res.chance === 1){
